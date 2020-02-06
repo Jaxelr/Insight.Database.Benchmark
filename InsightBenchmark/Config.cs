21f9c0d2e1d8b7eff7f0ead4fdbc07ebf096e3d0
Jaxel Rojas <jaxelr@gmail.com>
2020-02-06 12:00:39 -0400

    chore: added the custom config for benchmarks

---
+++ InsightBenchmark/Config.cs
@@ -0,0 +1,41 @@
+﻿using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Exporters;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Loggers;
+using BenchmarkDotNet.Order;
+
+namespace InsightBenchmark
+{
+    public class Config : ManualConfig
+    {
+        public Config()
+        {
+            Add(ConsoleLogger.Default);
+            Add(MarkdownExporter.GitHub);
+
+            var md = MemoryDiagnoser.Default;
+            Add(md);
+
+            Add(TargetMethodColumn.Method);
+            Add(TargetMethodColumn.Type);
+            Add(StatisticColumn.Mean);
+            Add(StatisticColumn.StdDev);
+            Add(StatisticColumn.Error);
+            Add(StatisticColumn.Min);
+            Add(StatisticColumn.Max);
+            Add(StatisticColumn.OperationsPerSecond);
+            Add(DefaultColumnProviders.Metrics);
+
+            Add(Job.ShortRun
+             .WithLaunchCount(10)
+             .WithWarmupCount(20)
+             .WithMaxIterationCount(100)
+             .WithIterationCount(10));
+
+            Orderer = new DefaultOrderer(SummaryOrderPolicy.FastestToSlowest);
+            Options |= ConfigOptions.JoinSummary;
+        }
+    }
+}
--- InsightBenchmark/Program.cs
+++ InsightBenchmark/Program.cs
@@ -1,27 +1,12 @@
-﻿using BenchmarkDotNet.Configs;
-using BenchmarkDotNet.Running;
-using Microsoft.Extensions.Configuration;
-using System.IO;
+﻿using BenchmarkDotNet.Running;
 
 namespace InsightBenchmark
 {
-    internal class Program
+    internal static class Program
     {
-        public static string ConnectionString;
-        public static int Iterations;
-
         private static void Main(string[] args)
         {
-            var builder = new ConfigurationBuilder()
-           .SetBasePath(Directory.GetCurrentDirectory())
-           .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
-
-            IConfigurationRoot configuration = builder.Build();
-
-            ConnectionString = configuration.GetConnectionString("Default");
-            Iterations = int.Parse(configuration.GetSection("Records").Value);
-
-            BenchmarkSwitcher.FromAssembly(typeof(InsightBenchmark).Assembly).Run(args, new DebugInProcessConfig());
+            BenchmarkSwitcher.FromAssembly(typeof(InsightBenchmark).Assembly).Run(args, new Config());
         }
     }
 }
