9d7c091784712f72d16ec2c9e9f35ee1b1c1c882
Jaxel Rojas <jaxelr@gmail.com>
2020-03-31 17:45:06 -0400

    style: minor refactor

--- benchmark/Program.cs
+++ benchmark/Program.cs
@@ -4,9 +4,9 @@ namespace Insight.Database.Benchmark
 {
     internal static class Program
     {
-        private static void Main(string[] args)
-        {
-            BenchmarkSwitcher.FromAssembly(typeof(InsightBenchmark).Assembly).Run(args, new Config());
-        }
+        private static void Main(string[] args) =>
+            _ = BenchmarkSwitcher
+                .FromAssembly(typeof(InsightBenchmark).Assembly)
+                .Run(args, new Config());
     }
 }
