99a486b154689957e91e8c8735b3e6e2f325e559
Jaxel Rojas <jaxelr@gmail.com>
2020-12-30 11:21:02 -0400

    refactor: moved write operations to own class

--- benchmark/InsightBenchmark.cs
+++ benchmark/InsightBenchmark.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
@@ -12,11 +11,6 @@ namespace Insight.Database.Benchmark
     public class InsightBenchmark : BaseBenchmark
     {
         protected SqlConnection connection;
-
-        public static IEnumerable<Post> Posts()
-        {
-            yield return new Post() { Text = new string('x', 2000), CreationDate = DateTime.Now, LastChangeDate = DateTime.Now };
-        }
 
         [Benchmark(Description = "Single")]
         [BenchmarkCategory("Read")]
@@ -91,35 +85,6 @@ namespace Insight.Database.Benchmark
                 ("SELECT Id [Item1], [Text] [Item2], CreationDate [Item3] FROM Post WHERE Id = @param", new { param });
 
             return result.FirstOrDefault();
-        }
-
-        [Benchmark(Description = "Insert<T>")]
-        [BenchmarkCategory("Write")]
-        [ArgumentsSource(nameof(Posts))]
-        public Post InsertPost(Post post) => connection.InsertSql("INSERT INTO Post (Text, CreationDate, LastChangeDate) VALUES (@Text, @CreationDate, @LastChangeDate) ", post);
-
-        [Benchmark(Description = "Insert<T> Async")]
-        [BenchmarkCategory("Write")]
-        [ArgumentsSource(nameof(Posts))]
-        public async Task<Post> InsertPostAsync(Post post) =>
-            await connection.InsertSqlAsync("INSERT INTO Post (Text, CreationDate, LastChangeDate) VALUES (@Text, @CreationDate, @LastChangeDate)", post);
-
-        [Benchmark(Description = "Update<T>")]
-        [BenchmarkCategory("Write")]
-        [ArgumentsSource(nameof(Posts))]
-        public Post UpdatePost(Post post)
-        {
-            post.Id = param;
-            return connection.QueryOntoSql("UPDATE Post SET Text = @Text, CreationDate = @CreationDate, LastChangeDate = @LastChangeDate output inserted.* WHERE Id = @Id", post);
-        }
-
-        [Benchmark(Description = "Update<T> Async")]
-        [BenchmarkCategory("Write")]
-        [ArgumentsSource(nameof(Posts))]
-        public async Task<Post> UpdatePostAsync(Post post)
-        {
-            post.Id = param;
-            return await connection.QueryOntoSqlAsync("UPDATE Post SET Text = @Text, CreationDate = @CreationDate, LastChangeDate = @LastChangeDate output inserted.* WHERE Id = @Id", post);
         }
 
         [Benchmark(Description = "Query<T>")]
