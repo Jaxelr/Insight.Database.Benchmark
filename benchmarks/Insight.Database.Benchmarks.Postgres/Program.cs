23ffcf91ac9b5296f67579bdd6743aa77c3fd749
Jaxel Rojas <jaxelr@gmail.com>
2021-03-18 23:30:03 -0400

    fix: removed console.Read();

--- benchmarks/Insight.Database.Benchmarks.Postgres/Program.cs
+++ benchmarks/Insight.Database.Benchmarks.Postgres/Program.cs
@@ -1,17 +1,13 @@
-﻿using System;
-using BenchmarkDotNet.Running;
+﻿using BenchmarkDotNet.Running;
 
 namespace Insight.Database.Benchmarks.Postgres
 {
     internal static class Program
     {
-        private static void Main(string[] args)
-        {
+        private static void Main(string[] args) =>
+
             _ = BenchmarkSwitcher
                 .FromAssembly(typeof(InsightBenchmark).Assembly)
                 .Run(args, new Config());
-
-            Console.Read();
-        }
     }
 }
