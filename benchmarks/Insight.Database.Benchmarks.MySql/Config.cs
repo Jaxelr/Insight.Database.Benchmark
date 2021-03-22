f48ad0b50fc3386d6e86341b93a2b9d1ae1846c5
Jaxel Rojas <jaxelr@gmail.com>
2021-03-22 16:01:04 -0400

    feat: added initial prj with mysql references

---
+++ benchmarks/Insight.Database.Benchmarks.MySql/Config.cs
@@ -0,0 +1,42 @@
+﻿using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Exporters;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Loggers;
+using BenchmarkDotNet.Order;
+
+namespace Insight.Database.Benchmarks.MySql
+{
+    public class Config : ManualConfig
+    {
+        public Config()
+        {
+            AddLogger(ConsoleLogger.Default);
+            AddExporter(MarkdownExporter.GitHub);
+
+            var md = MemoryDiagnoser.Default;
+            AddDiagnoser(md);
+
+            AddColumn(TargetMethodColumn.Method);
+            AddColumn(TargetMethodColumn.Type);
+            AddColumn(StatisticColumn.Mean);
+            AddColumn(StatisticColumn.StdDev);
+            AddColumn(StatisticColumn.Error);
+            AddColumn(StatisticColumn.Min);
+            AddColumn(StatisticColumn.Max);
+            AddColumn(StatisticColumn.OperationsPerSecond);
+            AddColumnProvider(DefaultColumnProviders.Metrics);
+
+            AddJob(Job.ShortRun
+             .WithLaunchCount(1)
+             .WithWarmupCount(1)
+             .WithMinIterationCount(1)
+             .WithMaxIterationCount(5)
+             .WithIterationCount(1));
+
+            Orderer = new DefaultOrderer(SummaryOrderPolicy.FastestToSlowest);
+            Options |= ConfigOptions.JoinSummary;
+        }
+    }
+}
