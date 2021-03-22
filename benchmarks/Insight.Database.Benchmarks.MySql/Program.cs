4a21e2847206d7c0b465b0b6483aa9c8bb31f785
Jaxel Rojas <jaxelr@gmail.com>
2021-03-22 16:01:04 -0400

    feat: added initial prj with mysql references

---
+++ benchmarks/Insight.Database.Benchmarks.MySql/Program.cs
@@ -0,0 +1,13 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace Insight.Database.Benchmarks.MySql
+{
+    public static class Program
+    {
+        private static void Main(string[] args) =>
+
+    _ = BenchmarkSwitcher
+        .FromAssembly(typeof(BaseBenchmark).Assembly)
+        .Run(args, new Config());
+    }
+}
