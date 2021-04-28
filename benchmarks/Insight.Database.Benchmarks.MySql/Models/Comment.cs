4b44f800cb9bdd0bac2d5a41720af6f402b4d9a3
Jaxel Rojas <jaxelr@gmail.com>
2021-04-28 16:19:03 -0400

    feat: added benchmark implementation for mysql

---
+++ benchmarks/Insight.Database.Benchmarks.MySql/Models/Comment.cs
@@ -0,0 +1,9 @@
+﻿namespace Insight.Database.Benchmarks.MySql.Models
+{
+    public class Comment
+    {
+        public int PostId { get; set; }
+        public int Id { get; set; }
+        public string CommentText { get; set; }
+    }
+}
--- benchmarks/Insight.Database.Benchmarks.MySql/Program.cs
+++ benchmarks/Insight.Database.Benchmarks.MySql/Program.cs
@@ -5,9 +5,8 @@ namespace Insight.Database.Benchmarks.MySql
     public static class Program
     {
         private static void Main(string[] args) =>
-
-    _ = BenchmarkSwitcher
-        .FromAssembly(typeof(BaseBenchmark).Assembly)
-        .Run(args, new Config());
+            _ = BenchmarkSwitcher
+                .FromAssembly(typeof(BaseBenchmark).Assembly)
+                .Run(args, new Config());
     }
 }
