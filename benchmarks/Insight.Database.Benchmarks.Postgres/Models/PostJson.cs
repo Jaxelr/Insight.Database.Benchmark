b5c25b537d094c5d4c5a226bbecafd4c3d6d14ff
Jaxel Rojas <jaxelr@gmail.com>
2021-05-21 20:39:06 -0400

    feat: implementation of json benchmarks for postgresql

---
+++ benchmarks/Insight.Database.Benchmarks.Postgres/Models/PostJson.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Insight.Database.Benchmarks.Postgres.Models
+{
+    public class PostJson
+    {
+        public int Id { get; set; }
+
+        [Column(SerializationMode = SerializationMode.Json)]
+        public ChildJson Child { get; set; }
+
+        public DateTime CreationDate { get; set; }
+        public DateTime LastChangeDate { get; set; }
+    }
+
+    public class ChildJson
+    {
+        public string Text { get; set; }
+    }
+}
