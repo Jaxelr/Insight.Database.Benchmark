5431480dd296fb1c5739df3db756da2a748fd112
Jaxel Rojas <jaxelr@gmail.com>
2021-07-06 18:27:00 -0400

    fix: corrected scripts for global setup

--- benchmarks/Insight.Database.Benchmarks.Sqlite/InsightBenchmark.cs
+++ benchmarks/Insight.Database.Benchmarks.Sqlite/InsightBenchmark.cs
@@ -153,7 +153,7 @@ namespace Insight.Database.Benchmarks.Sqlite
         
                     CREATE TABLE IF NOT EXISTS [Post] 
                     (
-                      Id INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
+                        Id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
                         [Text] VARCHAR NOT NULL,
                         CreationDate DATETIME NOT NULL,
                         LastChangeDate DATETIME NOT NULL
@@ -161,31 +161,42 @@ namespace Insight.Database.Benchmarks.Sqlite
         
                     CREATE TABLE IF NOT EXISTS [Comment]
                     (
-                        Id INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
-                        PostId INT NOT NULL,
+                        Id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+                        PostId INTEGER NOT NULL,
                         [CommentText] VARCHAR NOT NULL,
                         CreationDate DATETIME NOT NULL
                     );
         
                     DELETE FROM Comment;
                     DELETE FROM Post;
-";
 
-            //DECLARE @i INT = 0;
-            //        WHILE (@i <= {iterations})
-            //        BEGIN
-            //            DECLARE @PostId INT;
 
-            //            INSERT INTO	Post([Text], CreationDate, LastChangeDate)
-            //            SELECT REPLICATE('x', 2000), SYSDATETIME(), SYSDATETIME();
-            //            SET @i = @i + 1;
+                    WITH RECURSIVE temp ([Text], [CreationDate], [LastChangeDate])
+                    AS 
+                    (
+                        SELECT 
+                            PRINTF('%.' || 2000 ||'c', 'X') [Text],
+                            DATETIME('now') [CreationDate], 
+                            DATETIME('now') [LastChangeDate]
+                        UNION ALL
+                        SELECT 
+                            [Text], 
+                            [CreationDate],
+                            [LastChangeDate]
+                        FROM temp
+                        LIMIT {iterations}
+    
+                    )
 
-            //            SELECT @PostId = SCOPE_IDENTITY();
+                    INSERT INTO Post([Text], [LastChangeDate], [CreationDate])
+                    SELECT [Text], [LastChangeDate], [CreationDate] FROM temp;
 
-            //            INSERT INTO	Comment([CommentText], CreationDate, PostId)
-            //            SELECT REPLICATE('x', 2000), SYSDATETIME(), @PostId UNION ALL
-            //            SELECT REPLICATE('x', 2000), SYSDATETIME(), @PostId
-            //        END;";
+                    INSERT INTO Comment([PostId], [CommentText], [CreationDate])
+                    SELECT Id PostId, PRINTF('%.' || 2000 ||'c', 'X') [Text], DATETIME('now') [CreationDate]
+                    FROM Post
+                    UNION ALL 
+                    SELECT Id PostId, PRINTF('%.' || 2000 ||'c', 'X') [Text], DATETIME('now') [CreationDate]
+                    FROM Post";
 
             cmd.Connection = connection;
             cmd.ExecuteNonQuery();
