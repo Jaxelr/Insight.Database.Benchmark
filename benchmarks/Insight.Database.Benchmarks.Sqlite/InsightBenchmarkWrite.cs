a7ad58662e1cefe2fc21748e6bd546e17be74790
Jaxel Rojas <jaxelr@gmail.com>
2021-07-09 17:37:15 -0400

    feat: added writing operations for sqlite

---
+++ benchmarks/Insight.Database.Benchmarks.Sqlite/InsightBenchmarkWrite.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using Insight.Database.Benchmarks.Sqlite.Models;
+
+namespace Insight.Database.Benchmarks.Sqlite
+{
+    public class InsightBenchmarkWrite : InsightBenchmark
+    {
+        public static IEnumerable<Post> Posts()
+        {
+            yield return new Post() { Text = new string('x', 2000), CreationDate = DateTime.Now, LastChangeDate = DateTime.Now };
+        }
+
+        [Benchmark(Description = "Insert<T>")]
+        [BenchmarkCategory("Write")]
+        [ArgumentsSource(nameof(Posts))]
+        public Post InsertPost(Post post) => connection.InsertSql("INSERT INTO Post (Text, CreationDate, LastChangeDate) VALUES (@Text, @CreationDate, @LastChangeDate); SELECT last_insert_rowid() as Id", post);
+
+        [Benchmark(Description = "Insert<T> Async")]
+        [BenchmarkCategory("Write")]
+        [ArgumentsSource(nameof(Posts))]
+        public async Task<Post> InsertPostAsync(Post post) =>
+            await connection.InsertSqlAsync("INSERT INTO Post (Text, CreationDate, LastChangeDate) VALUES (@Text, @CreationDate, @LastChangeDate); ; SELECT last_insert_rowid() as Id", post);
+
+        [Benchmark(Description = "Update<T>")]
+        [BenchmarkCategory("Write")]
+        [ArgumentsSource(nameof(Posts))]
+        public Post UpdatePost(Post post)
+        {
+            post.Id = param;
+            return connection.QueryOntoSql("UPDATE Post SET Text = @Text, CreationDate = @CreationDate, LastChangeDate = @LastChangeDate WHERE Id = @Id; SELECT last_insert_rowid() as Id", post);
+        }
+
+        [Benchmark(Description = "Update<T> Async")]
+        [BenchmarkCategory("Write")]
+        [ArgumentsSource(nameof(Posts))]
+        public async Task<Post> UpdatePostAsync(Post post)
+        {
+            post.Id = param;
+            return await connection.QueryOntoSqlAsync("UPDATE Post SET Text = @Text, CreationDate = @CreationDate, LastChangeDate = @LastChangeDate WHERE Id = @Id; SELECT last_insert_rowid() as Id", post);
+        }
+    }
+}
