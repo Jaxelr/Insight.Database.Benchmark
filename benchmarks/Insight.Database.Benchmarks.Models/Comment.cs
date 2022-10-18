34d2bae2c6d4ec2a783063dcbd5e1b897f5a25ac
Jaxel Rojas <jaxelr@gmail.com>
2022-10-18 05:53:30 -0400

    style: normalized namespaces

--- benchmarks/Insight.Database.Benchmarks.Models/Comment.cs
+++ benchmarks/Insight.Database.Benchmarks.Models/Comment.cs
@@ -1,9 +1,8 @@
-﻿namespace Insight.Database.Benchmarks.Models
+﻿namespace Insight.Database.Benchmarks.Models;
+
+public class Comment
 {
-    public class Comment
-    {
-        public int PostId { get; set; }
-        public int Id { get; set; }
-        public string CommentText { get; set; }
-    }
+    public int PostId { get; set; }
+    public int Id { get; set; }
+    public string CommentText { get; set; }
 }
